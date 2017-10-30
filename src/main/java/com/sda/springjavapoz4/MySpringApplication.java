408ca1da04a2e002f45d8bd60a98084528f95d8b
RENT <damiasz@op.pl>
2017-10-30 19:08:59 +0100

    Spting introduction

---
+++ src/main/java/com/sda/springjavapoz4/MySpringApplication.java
@@ -0,0 +1,25 @@
+package com.sda.springjavapoz4;
+
+import com.sda.springjavapoz4.service.SomeService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+@SpringBootApplication
+public class MySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringApplication.class, args);
+	}
+	@Bean
+	public SomeService someService(){
+		return new SomeService("My secret message");
+	}
+
+	@Primary
+	@Bean
+	public SomeService aboutService(){
+		return new SomeService("About section");
+	}
+}
---
+++ src/main/java/com/sda/springjavapoz4/service/SomeService.java
@@ -0,0 +1,24 @@
+package com.sda.springjavapoz4.service;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+//@Component
+
+public class SomeService {
+
+    private String message;
+    private int value;
+
+    public SomeService(String message) {
+        Random random = new Random();
+        this.value = random.nextInt(100);
+        this.message = message;
+    }
+
+
+    public void someAction(){
+        System.out.printf(message + " " + value + "\n");
+    }
+}
